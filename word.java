cbc3dd6ea02cda46629983dbf95c674f017abfd8
pavuluri yedukondalu <96153682+kondalu98@users.noreply.github.com>
2023-02-24 21:10:00 +0530

    Update word.java

--- word.java
+++ word.java
@@ -1,24 +1,39 @@
-import java.security.KeyStore.Entry;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
-public class Word {
+class one {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-        String words[] = text.split(" ");
+        HashMap<String, Integer> hs = new HashMap<>();
+        String s = sc.nextLine();
+        String[] sr = s.split(" ");
+        
 
-        HashMap<String,Integer> frequency = new HashMap<>();
-        for(String word: words){
-            if(frequency.containsKey(word)){
-                frequency.put(word, frequency.get(word)+1);
-            }else{
-                frequency.put(word,1);
+        for (int i = 0; i < sr.length; i++) 
+        {
+            int c = 0;
+
+            for (int j = 0; j < sr.length; j++)
+             {
+
+                if (sr[i].equals(sr[j])) {
+                    c++;
+
+                }
             }
+            hs.put(sr[i], c);
+            
+
         }
-        for(Map.Entry<String,Integer>entry:frequency.entrySet()){
-            System.out.println(entry.getKey()+" : "+entry.getValue());
-        }
+        Set se=hs.keySet();
+       for(Object ob: se)
+       {
+        Object obb=hs.get(ob);
+        System.out.println(obb+"\t"+ob);
+       }
     }
+
 }
